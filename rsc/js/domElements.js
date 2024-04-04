943a86f56340a9e2ee5db830b8843bd0cc60d536
Steve <steve.gitprofile@outlook.com>
2024-04-04 16:17:17 +0200

    added modal functionality and smooth scrolling

--- rsc/js/domElements.js
+++ rsc/js/domElements.js
@@ -1,6 +1,16 @@
 export const domElements = {
+  // header elements
+  headerEl: document.querySelector('.header'),
+  // modal elements
   modal: document.querySelector('.modal'),
   overlay: document.querySelector('.overlay'),
   btnCloseModal: document.querySelector('.btn--close-modal'),
   btnsOpenModal: document.querySelectorAll('.btn--show-modal'),
+  // learn more btn
+  btnScrollTo: document.querySelector('.btn--scroll-to'),
+  // sections
+  sectionOne: document.querySelector('#section--1'),
+  // nav elements
+  allNavLinks: document.querySelectorAll('.nav__link'),
+  navUlEl: document.querySelector('.nav__links'),
 };
--- rsc/js/script.js
+++ rsc/js/script.js
@@ -1,7 +1,30 @@
 'use strict';
 
-//  imports
+// ------------------------------------------------
+//  IMPORTS
+// ------------------------------------------------
+
 import { domElements } from './domElements.js';
+
+// ------------------------------------------------
+// VARIABLES
+// ------------------------------------------------
+
+const bgColDark = '#37383d';
+
+// ------------------------------------------------
+// ELEMENT CREATION
+// ------------------------------------------------
+
+// ------- create a cookie message
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn btn--close--cookie">Got it!</button>`;
+
+domElements.headerEl.append(message);
+message.style.backgroundColor = bgColDark;
+message.style.width = '80%';
+
 // ------------------------------------------------
 // FUNCTIONS
 // ------------------------------------------------
@@ -23,15 +46,65 @@ const closeModal = function () {
 
 // ------- modal eventListeners
 
+// open
 domElements.btnsOpenModal.forEach(btnM =>
   btnM.addEventListener('click', openModal)
 );
 
+// close click button
 domElements.btnCloseModal.addEventListener('click', closeModal);
 domElements.overlay.addEventListener('click', closeModal);
 
+// close esc key
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !domElements.modal.classList.contains('hidden')) {
     closeModal();
+  }
+});
+
+// ------- cookie eventListeners
+document
+  .querySelector('.btn--close--cookie')
+  .addEventListener('click', () => message.remove()); // not use domElements not on DOM when imported
+
+// ------- learn more (smooth scroll)
+domElements.btnScrollTo.addEventListener('click', e => {
+  // ! need to add scroll-behaviour property on html element in css
+  e.preventDefault();
+  // old approach
+  const sectionOneCoords = domElements.sectionOne.getBoundingClientRect(); // get co-ordinates for element want to scroll to
+  window.scrollTo({
+    left: sectionOneCoords.left,
+    top: sectionOneCoords.top + window.scrollY, // need to add current scroll to top otherwise wont work
+    behavior: 'smooth',
+  });
+
+  // modern approach (only works on latest browsers)
+  /* domElements.sectionOne.scrollIntoView({ behaviour: 'smooth' }); */
+});
+
+// ------- navigation (smooth scroll)
+
+//  ! without delegation (add too many unecessary eventHandlers, impact performance)
+// adds second event handler for open account link/button
+/* domElements.allNavLinks.forEach(navLink => {
+  navLink.addEventListener('click', e => {
+    e.preventDefault();
+    const sectionId = e.target.getAttribute('href'); // instead of this as no this in arrow
+    document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+  });
+}); */
+
+// ! event delegation add one event listener to common parent element
+domElements.navUlEl.addEventListener('click', e => {
+  e.preventDefault();
+  // get matching strategy
+  if (
+    e.target.classList.contains('nav__link') &&
+    !e.target.classList.contains('nav__link--btn') // exclude the open account nav__link
+  ) {
+    const targetId = e.target.getAttribute('href');
+    console.log(document.querySelector(targetId));
+    document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
   }
 });
